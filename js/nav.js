d6106e13373ee14ac561e413e0d65e4f4f314000
Manas-P <62294926+Manas-P@users.noreply.github.com>
2020-04-10 17:18:30 +0530

    Background Changed

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,13 @@
 const navSlide = () => {
   const nav_btn = document.querySelector(".nav_btn");
   const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll("nav-links li");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
   nav_btn.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
 
     nav_btn.classList.toggle(`toggle`);
+    navLinks.classList.toggle("nav-active");
   });
 };
 
